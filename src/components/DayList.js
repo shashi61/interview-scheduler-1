6373184d876d8c1eaebc4a9624f3e362f47da318
Shashi Bawa <shashibawa104@gmail.com>
2022-01-27 17:20:21 -0500

    Add new file for Day List

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -3,27 +3,28 @@ import DayListItem from 'components/DayListItem';
 
 
 const DayList = (props) => {
- 
-  const arrayOfDays = props.days;
-  const parsedDayListItems = arrayOfDays.map(dayListItem => 
-    { return (
-      
-    <ul key={dayListItem.id} > 
-    <DayListItem
-        key={dayListItem.id}  
-        name={dayListItem.name} 
-        selected={dayListItem.name === props.value}
-        spots={dayListItem.spots} 
-        setDay={props.onChange} 
-    // setDay={props.setDay} 
-    /></ul>)
 
-  })
-  return (
-    <li>
-       { parsedDayListItems }
-    </li>
-  )
+    const arrayOfDays = props.days;
+    const parsedDayListItems = arrayOfDays.map(dayListItem => {
+
+        return (
+            <ul key={dayListItem.id} >
+                <DayListItem
+                    key={dayListItem.id}
+                    name={dayListItem.name}
+                    spots={dayListItem.spots}
+                    selected={dayListItem.name === props.day}
+                    // setDay={props.onChange}
+                    setDay={props.setDay} 
+                />
+            </ul>)
+
+    })
+    return (
+        <li>
+            {parsedDayListItems}
+        </li>
+    )
 }
 
 export default DayList
