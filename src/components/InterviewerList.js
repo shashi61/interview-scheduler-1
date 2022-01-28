5b50174f57ef8043e340cf8c0a1166e90a2fd27e
Shashi Bawa <shashibawa104@gmail.com>
2022-01-28 01:00:16 -0500

    Craete InterviewerList component to hold list of interviewers

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import InterviewerListItem from 'components/InterviewerListItem';
+
+const InterviewerList = (props) => {
+
+    <section className="interviewers">
+        <h4 className="interviewers__header text--light">Interviewer</h4>
+        <ul className="interviewers__list"></ul>
+    </section>
+}
+
+export default InterviewerList;
