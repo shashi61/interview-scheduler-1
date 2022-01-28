09403231a2c0e4b592daf4d5208aec9d0dc80dcb
Shashi Bawa <shashibawa104@gmail.com>
2022-01-27 22:47:32 -0500

    Create interviewerListItem component and export in stories

--- stories/index.js
+++ stories/index.js
@@ -8,6 +8,7 @@ import "index.scss";
 import Button from "components/Button";
 import DayListItem from "components/DayListItem";
 import DayList from "components/DayList";
+import InterviewListItem from "components/DayListItem";
 
 storiesOf("Button", module)
   .addParameters({
@@ -67,3 +68,14 @@ storiesOf("Button", module)
     .add("Wednesday", () => (
         <DayList days={days} day={"Wednesday"} setDay={action("setDay")} />
     ));
+
+  storiesOf("InterviewListItem", module) //Initiates Storybook and registers our DayListItem component
+  .addParameters({
+    backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+  }) // Provides the default background color for our component
+  .add("Unselected", () => <InterviewListItem name="Monday" spots={5} />) // To define our stories, we call add() once for each of our test states to generate a story
+  .add("Selected", () => <InterviewListItem name="Monday" spots={5} selected />) 
+  .add("Full", () => <InterviewListItem name="Monday" spots={0} />)
+  .add("Clickable", () => (
+    <DInterviewListItem name="Tuesday" setInterviewer={action("setInterviewer")} spots={5} /> // action() allows us to create a callback that appears in the actions panel when clicked
+  ));
