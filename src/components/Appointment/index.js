0ecc57686709ea6c17c98f48eee84ea106537210
Shashi Bawa <shashibawa104@gmail.com>
2022-01-28 02:08:48 -0500

    Create appointment component and styles file for it

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "src/components/Appointment/styles.scss";
+
+const Appointment = (props) => {
+
+    return (
+        <article className="appointment"></article>
+    );
+}
+export default Appointment;
