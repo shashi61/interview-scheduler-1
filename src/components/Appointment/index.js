4012f5095ffd01d0bd2c34ce6e25ca2d5885e9f7
Shashi Bawa <shashibawa104@gmail.com>
2022-01-29 19:01:53 -0500

    Display appointments

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { Fragment } from 'react'
 import "components/Appointment/styles.scss";
+import Header from "./Header";
+import Empty from "./Empty";
+import Show from "./Show";
+import Form from "./Form";
+// import useVisualMode from "hooks/useVisualMode";
+import Status from "./Status";
+import Confirm from "./Confirm";
+import Error from "./Error";
 
 const Appointment = (props) => {
-
+  
     return (
-        <article className="appointment"></article>
+        <article className="appointment" > 
+            <Fragment>
+            <Header time={props.time}/>
+                {
+                    props.interview ?
+                    <>
+
+                    <Show student={props.interview.student} interviewer={props.interview.interviewer.name}/>
+                    </>
+                    :
+                    <>
+                    <Header />
+                    <Empty />
+                    </>
+                }
+            </Fragment>
+        </article>
     );
 }
 export default Appointment;
